b4025b69f2b10966c57c5e8d878a0c6afdc6f521
OJBatex <simondr2006@gmail.com>
2024-02-18 15:38:31 -0500

    Intento 1

---
+++ main.py
@@ -0,0 +1,54 @@
+class Asiento:
+    def __init__(self, color, precio, registro):
+        self.color = color
+        self.precio = precio
+        self.registro = registro
+    
+    def cambiarColor(self, color):
+        if color == "rojo" or color == "verde" or color == "amarillo" or color == "negro" or color == "blanco":
+            self.color = color         
+
+class Motor:
+    def __init__(self, numeroCilindros, tipo, registro):
+        self.numeroCilindros = numeroCilindros
+        self.tipo = tipo
+        self.registro = registro
+
+    def cambiarRegistro(self, num):
+        if isinstance(num, int) == True:
+            self.registro = num
+
+    def asignarTipo(self, tipo):
+        if tipo == "electrico" or tipo == "gasolina":
+            self.tipo = tipo
+
+class Atuto:
+    cantidadCreados = 0
+    def __init__(self, modelo, precio, asientos, marca, motor, registro):
+        self.modelo = modelo
+        self.precio = precio
+        self.asientos = asientos
+        self.marca = marca
+        self.motor = motor
+        self.registro = registro
+
+    def cantidadAsientos(self):
+        cantAsientos = 0
+        for i in self.asientos:
+            if i != None:
+                cantAsientos += 1
+        return cantAsientos
+                
+
+    def verificarIntegridad(self):
+        if self.registro == self.motor.registro:
+            for asiento in self.asientos:
+                if asiento != None:
+                    if asiento.registro != self.registro:
+                        return "Las piezas no son originales"
+        return "Auto original"
+
+
+        
+
+
